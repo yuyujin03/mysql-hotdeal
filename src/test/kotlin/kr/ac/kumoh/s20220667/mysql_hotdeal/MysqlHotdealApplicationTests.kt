d9602fd6ea3571675d184027b87b9b9126876b65
yujin <youjinyeo12@gmail.com>
2025-06-21 19:12:20 +0900

    데이터베이스 생성

---
+++ src/test/kotlin/kr/ac/kumoh/s20220667/mysql_hotdeal/MysqlHotdealApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220667.mysql_hotdeal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysqlHotdealApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
