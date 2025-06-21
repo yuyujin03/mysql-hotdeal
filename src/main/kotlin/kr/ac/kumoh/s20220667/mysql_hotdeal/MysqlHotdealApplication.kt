025bd8d2079de8a3b8a90c50239e6a1ce9b30663
yujin <youjinyeo12@gmail.com>
2025-06-21 19:12:44 +0900

    데이터베이스 생성

---
+++ src/main/kotlin/kr/ac/kumoh/s20220667/mysql_hotdeal/MysqlHotdealApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20220667.mysql_hotdeal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysqlHotdealApplication
+
+fun main(args: Array<String>) {
+	runApplication<MysqlHotdealApplication>(*args)
+}
